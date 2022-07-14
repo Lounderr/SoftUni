2a3226fb569948a2ffa4ed1bdce42167254efb08
Lounderr <lounder044@gmail.com>
2022-07-14 13:57:02 +0300

    Finish Functional Programming

--- C# Advanced/05. Functional Programming/FunctionalProgramming/PartyReservationFilterModule/Program.cs
+++ C# Advanced/05. Functional Programming/FunctionalProgramming/PartyReservationFilterModule/Program.cs
@@ -1,31 +1,84 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace PartyReservationFilterModule
 {
+    public static class PRFM
+    {
+        private static Dictionary<string, Predicate<string>> filters = new Dictionary<string, Predicate<string>>();
+
+        public static Predicate<string> allFiltersPassed = (person) =>
+        {
+            return filters.All(filter => !filter.Value(person));
+        };
+
+        public static void AddFilter(string filterType, string filterParameter)
+        {
+            if (filterType == "Starts with")
+            {
+                Predicate<string> filter = (person) => person.StartsWith(filterParameter);
+                filters.Add(filterType + filterParameter, filter);
+            }
+            else if (filterType == "Ends with")
+            {
+                Predicate<string> filter = (person) => person.EndsWith(filterParameter);
+                filters.Add(filterType + filterParameter, filter);
+            }
+            else if (filterType == "Length")
+            {
+                Predicate<string> filter = (person) => person.Length == int.Parse(filterParameter);
+                filters.Add(filterType + filterParameter, filter);
+            }
+            else if (filterType == "Contains")
+            {
+                Predicate<string> filter = (person) => person.Contains(filterParameter);
+                filters.Add(filterType + filterParameter, filter);
+            }
+        }
+        public static void RemoveFilter(string filterType, string filterParameter)
+        {
+            if (filterType == "Starts with")
+            {
+                filters.Remove(filterType + filterParameter);
+            }
+            else if (filterType == "Ends with")
+            {
+                filters.Remove(filterType + filterParameter);
+            }
+            else if (filterType == "Length")
+            {
+                filters.Remove(filterType + filterParameter);
+            }
+            else if (filterType == "Contains")
+            {
+                filters.Remove(filterType + filterParameter);
+            }
+        }
+
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            List<string> people = new List<string>();
+            List<string> people = Console.ReadLine().Split().ToList();
 
             while (true)
             {
-                string[] cmd = Console.ReadLine().Split();
+                string[] cmd = Console.ReadLine().Split(';');
 
                 if (cmd[0] == "Print")
                 {
-                    // TODO: Print
+                    Console.WriteLine(String.Join(' ', people.Where(person => PRFM.allFiltersPassed(person))));
                     break;
                 }
-
                 else if (cmd[0] == "Add filter")
                 {
-                    // TODO: Add filter
+                    PRFM.AddFilter(cmd[1], cmd[2]);
                 }
                 else if (cmd[0] == "Remove filter")
                 {
-                    // TODO: Remove filter
+                    PRFM.RemoveFilter(cmd[1], cmd[2]);
                 }
             }
         }
---
+++ C# Advanced/05. Functional Programming/FunctionalProgramming/TriFunction/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace TriFunction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] names = Console.ReadLine().Split();
+
+            int counter = 0;
+
+            for (int i = 0; i < names.Length;)
+            {
+                var name = names[i];
+                for (int j = 0; j < name.Length; j++)
+                {
+                    if (counter >= n)
+                    {
+                        Console.WriteLine(name);
+                        return;
+                    }
+                    counter += names[i][j];
+                }
+
+                i++;
+                if (!(i < names.Length))
+                    i = 0;
+            }
+        }
+    }
+}
