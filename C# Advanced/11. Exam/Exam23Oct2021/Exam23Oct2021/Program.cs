647c6088c295832ce4dd50deb9a60a7363fc27b8
Lounderr <lounder044@gmail.com>
2022-08-21 00:27:00 +0300

    C# Advanced - Exercises

---
+++ C# Advanced/11. Exam/Exam23Oct2021/Exam23Oct2021/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exam23Oct2021
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // vowels sequence
+            // consonants sequence
+
+            // check if the words {words} can be created
+            // words = { "pear", "flour", "pork", "olive" }
+
+            // vf = vowels.First(); cl = consonants.Last(); 
+            // if (vf in words || cl in words) then store
+            // repeat until consonants.Len = 0
+            // letters can be in one or more words
+
+            // print words found
+            Queue<char> vowels = new Queue<char>(Console.ReadLine().Split(' ').Select(char.Parse));
+
+            Stack<char> consonants = new Stack<char>(Console.ReadLine().Split(' ').Select(char.Parse));
+
+            Stack<char> consonantsCopy = new Stack<char>(consonants);
+
+            Dictionary<string, int> matchesByWords = new Dictionary<string, int>()
+            {
+                { "pear", 0 },
+                { "flour", 0 },
+                { "pork", 0 },
+                { "olive", 0 }
+            };
+
+            List<string> matchingWords = new List<string>();
+
+
+            foreach (var word in matchesByWords.Keys.ToArray())
+            {
+                for (int i = 0; i < word.Length; i++)
+                {
+                    while (consonants.Count() > 0)
+                    {
+                        if (word[i] == consonants.Peek() || word[i] == vowels.Peek())
+                        {
+                            //Console.WriteLine(word + "-" + word[i] + "#" + i + "$" + consonants.Peek() + "/" + vowels.Peek());
+                            matchesByWords[word]++;
+                            break;
+                        }
+
+                        vowels.Enqueue(vowels.Dequeue());
+                        consonants.Pop();
+                    }
+
+                    consonants = new Stack<char>(consonantsCopy);
+                }
+
+                if (matchesByWords[word] == word.Length)
+                {
+                    matchingWords.Add(word);
+                }
+            }
+
+            Console.WriteLine("Words found: " + matchingWords.Count);
+            foreach (var word in matchingWords)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
---
+++ C# Advanced/11. Exam/Exam23Oct2021/P02.PawnWars/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Linq;
+
+namespace P02.PawnWars
+{
+    internal class Program
+    {
+        public class Point
+        {
+            public int Row;
+            public int Col;
+
+            public Point(int row, int col)
+            {
+                Row = row;
+                Col = col;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            // read input -> init matrix AND b/w positions
+            // w makes the first move
+            // both pawns move until:
+            // a) they are diagonal to each other -> next wins 
+            // b) one reaches the end -> current wins
+
+
+            // Fill matrix and get pawn positions
+            Point wPawn = new Point(-1, -1);
+            Point bPawn = new Point(-1, -1);
+
+            char[,] matrix = new char[8, 8];
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                char[] inputRow = Console.ReadLine().ToCharArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    char squareValue = inputRow[col];
+                    matrix[row, col] = squareValue;
+
+                    if (squareValue == 'w')
+                    {
+                        wPawn = new Point(row, col);
+                    }
+                    else if (squareValue == 'b')
+                    {
+                        bPawn = new Point(row, col);
+                    }
+                }
+            }
+
+            bool isWhiteTurn = true;
+            while (true)
+            {
+                if (isWhiteTurn)
+                {
+                    if (IsDiagonal(wPawn, bPawn))
+                    {
+                        Console.WriteLine($"Game over! White capture on {(char)(bPawn.Col + 97)}{8 - bPawn.Row}.");
+                        return;
+                    }
+
+                    if (wPawn.Row == 0)
+                    {
+                        Console.WriteLine($"Game over! White pawn is promoted to a queen at {(char)(wPawn.Col + 97)}{8 - wPawn.Row}.");
+                        return;
+                    }
+
+                    matrix[wPawn.Row, wPawn.Col] = '-';
+                    matrix[wPawn.Row - 1, wPawn.Col] = 'w';
+                    wPawn.Row--;
+
+                    isWhiteTurn = false;
+                }
+                else
+                {
+                    if (IsDiagonal(wPawn, bPawn))
+                    {
+                        Console.WriteLine($"Game over! Black capture on {(char)(wPawn.Col + 97)}{8 - wPawn.Row}.");
+                        return;
+                    }
+
+                    if (bPawn.Row == matrix.GetLength(0) - 1)
+                    {
+                        Console.WriteLine($"Game over! Black pawn is promoted to a queen at {(char)(bPawn.Col + 97)}{8 - bPawn.Row}.");
+                        return;
+                    }
+
+                    matrix[bPawn.Row, bPawn.Col] = '-';
+                    matrix[bPawn.Row + 1, bPawn.Col] = 'b';
+                    bPawn.Row++;
+
+                    isWhiteTurn = true;
+                }
+
+                //Print(matrix);
+            }
+        }
+
+        private static bool IsDiagonal(Point wPawn, Point bPawn)
+        {
+            return bPawn.Row == wPawn.Row - 1 && (bPawn.Col == wPawn.Col - 1 || bPawn.Col == wPawn.Col + 1);
+        }
+
+        //private static void Print(char[,] matrix)
+        //{
+        //    Console.WriteLine("\n\n");
+        //    for (int row = 0; row < matrix.GetLength(0); row++)
+        //    {
+        //        for (int col = 0; col < matrix.GetLength(1); col++)
+        //        {
+        //            Console.Write(matrix[row, col]);
+        //        }
+        //        Console.WriteLine();
+        //    }
+        //}
+    }
+}
