39e7ef6abc6bdfe64af064a76b7b37bb58ceaffb
Lounderr <77444667+Lounderr@users.noreply.github.com>
2023-01-21 17:49:00 +0200

    C# EFCore - AutoMapper - Exercises

---
+++ 04. Entity Framework Core/07.AutoMappingObjects/FastFood.Web/FastFood.Data/FastFoodContextDesignTimeFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FastFood.Data
+{
+    internal class FastFoodContextDesignTimeFactory : IDesignTimeDbContextFactory<FastFoodContext>
+    {
+        public FastFoodContext CreateDbContext(string[] args)
+        {
+            var builder = new DbContextOptionsBuilder<FastFoodContext>();
+            builder.UseSqlServer("Server=.;Database=FastFood;Trusted_Connection=True;MultipleActiveResultSets=true");
+            return new FastFoodContext(builder.Options);
+        }
+    }
+}
