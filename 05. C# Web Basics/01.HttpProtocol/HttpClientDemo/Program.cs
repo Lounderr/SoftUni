99c1efef8d07cfb4e940d45952fb6a5e7a0a3d4e
Lounderr <77444667+Lounderr@users.noreply.github.com>
2023-04-04 12:52:33 +0300

    C# Web Basics

---
+++ 05. C# Web Basics/01.HttpProtocol/HttpClientDemo/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace HttpClientDemo
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            const string NewLine = "\r\n";
+            TcpListener tcpListener = new TcpListener(IPAddress.Loopback, 12345);
+            tcpListener.Start();
+
+            // daemon // service
+            while (true)
+            {
+                var client = tcpListener.AcceptTcpClient();
+                using (var stream = client.GetStream())
+                {
+                    byte[] buffer = new byte[10000000];
+                    var length = stream.Read(buffer, 0, buffer.Length);
+
+                    string requestString =
+                        Encoding.UTF8.GetString(buffer, 0, length);
+                    Console.WriteLine(requestString);
+                    string html = "<h1>Hello world!!!!!!</h1>";
+                    string response = "HTTP/1.1 200 OK" + NewLine +
+                        "Server: GeorgiServer 2020" + NewLine +
+                        "Content-Type: text/html; charset=utf-8" + NewLine +
+                        "Content-Length: " + html.Length + NewLine +
+                        NewLine +
+                        html;
+
+                    byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+                    stream.Write(responseBytes);
+
+                    Console.WriteLine(new string('-', 70));
+                }
+            }
+        }
+    }
+}
---
+++ 05. C# Web Basics/02.AsynchronousProcessing/Demo/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Task.Run(async () => {
+                for (int i = 1; i <= 100; i++)
+                {
+                    HttpClient httpClient = new HttpClient();
+                    var url = $"https://vicove.com/vic-{i}";
+                    var httpResponse = await httpClient.GetAsync(url);
+                    var vic = await httpResponse.Content.ReadAsStringAsync();
+                    Console.WriteLine(vic.Length);
+                }
+            });
+
+            List<int> ints= new List<int>();
+
+            return;
+            
+            new Thread(() => {
+                HttpClient httpClient = new HttpClient();
+                for (int i = 1; i <= 100; i++)
+                {
+                    var url = $"https://vicove.com/vic-{i}";
+                    var httpResponse = httpClient.GetAsync(url).GetAwaiter().GetResult();
+                    var vic = httpResponse.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                    Console.WriteLine(vic.Length);
+                }
+            }).Start();
+
+            return;
+            HttpClient httpClient = new HttpClient();
+            for (int i = 1; i <= 100; i++)
+            {
+                var url = $"https://vicove.com/vic-{i}";
+                var httpResponse = httpClient.GetAsync(url).GetAwaiter().GetResult();
+                var vic = httpResponse.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                Console.WriteLine(vic.Length);
+            }
+        }
+
+
+        //static async Task Main(string[] args)
+        //{
+        //    Console.OutputEncoding = Encoding.UTF8;
+        //    TcpListener tcpListener = new TcpListener(
+        //        IPAddress.Loopback, 80);
+        //    tcpListener.Start();
+        //    while (true)
+        //    {
+        //        var client = tcpListener.AcceptTcpClient();
+        //        ProcessClientAsync(client);
+        //    }
+        //}
+
+    }
+}
